17fcb8cd1d2aed98eec62e1f9d95a190c14d886d
martinpalsson <martin.palsson@outlook.com>
2024-12-15 15:31:37 +0100

    Added assertions on Packet level

--- src/lib.rs
+++ src/lib.rs
@@ -1167,6 +1167,20 @@ mod test {
         assert_eq!(repr, parsed_repr);
     }
 
+    fn round_trip_test_with_bytes(repr: Repr, expected_bytes: &[u8]) {
+        let mut buffer = vec![0u8; expected_bytes.len()];
+        {
+            let mut packet = Packet::new_unchecked(&mut buffer);
+            repr.emit(&mut packet);
+        }
+    
+        assert_eq!(&buffer, expected_bytes);
+    
+        let packet = Packet::new_checked(&buffer).unwrap();
+        let parsed_repr = Repr::parse(&packet).unwrap();
+        assert_eq!(repr, parsed_repr);
+    }
+
     #[test]
     fn test_repr_round_trip_generic_doip_header_negative_response() {
         let repr = Repr {
@@ -1179,6 +1193,9 @@ mod test {
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        ]);
     }
 
     #[test]
@@ -1193,6 +1210,9 @@ mod test {
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00
+        ]);
     }
 
     #[test]
@@ -1203,28 +1223,38 @@ mod test {
             payload: Payload {
                 type_code: PayloadTypeCode::VehicleIdentificationRequestEID,
                 length: field::payload::vir_eid::LENGTH as u32,
-                content: PayloadTypeContent::VehicleIdentificationRequestWithEID { eid: &[0; 6] },
+                content: PayloadTypeContent::VehicleIdentificationRequestWithEID { eid: &[0, 1, 2, 3, 4, 5] },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x00, 0x02, 0x00, 0x00, 0x00, 0x06, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05
+        ]);
     }
 
     #[test]
     fn test_repr_round_trip_vehicle_identification_request_vin() {
+        let vin_bytes = [89, 83, 51, 68, 68, 55, 56, 78, 52, 88, 55, 48, 53, 53, 51, 50, 48];
         let repr = Repr {
             protocol_version: 0x02,
             inverse_protocol_version: 0xfd,
             payload: Payload {
                 type_code: PayloadTypeCode::VehicleIdentificationRequestVIN,
                 length: field::payload::vir_vin::LENGTH as u32,
-                content: PayloadTypeContent::VehicleIdentificationRequestWithVIN { vin: &[0; 17] },
+                content: PayloadTypeContent::VehicleIdentificationRequestWithVIN { vin: &vin_bytes },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x00, 0x03, 0x00, 0x00, 0x00, 0x11, 89, 83, 51, 68, 68, 55, 56, 78, 52, 88, 55, 48, 53, 53, 51, 50, 48
+        ]);
     }
 
     #[test]
     fn test_repr_round_trip_vehicle_announcement_message() {
+        let vin_bytes = [83, 66, 77, 49, 50, 65, 66, 65, 51, 80, 87, 48, 48, 48, 48, 48, 49];
+    	let eid_bytes = [1, 2, 3, 4, 5, 6];
+        let gid_bytes = [6, 5, 4, 3, 2, 1];
         let repr = Repr {
             protocol_version: 0x02,
             inverse_protocol_version: 0xfd,
@@ -1232,16 +1262,19 @@ mod test {
                 type_code: PayloadTypeCode::VehicleAnnouncementMessage,
                 length: field::payload::vam::LENGTH as u32,
                 content: PayloadTypeContent::VehicleIdentificationResponseMessage {
-                    vin: &[0; 17],
+                    vin: &vin_bytes,
                     logical_address: 0,
-                    eid: &[0; 6],
-                    gid: &[0; 6],
+                    eid: &eid_bytes,
+                    gid: &gid_bytes,
                     further_action: 0,
                     gid_sync_status: 0,
                 },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x00, 0x04, 0x00, 0x00, 0x00, 0x21, 83, 66, 77, 49, 50, 65, 66, 65, 51, 80, 87, 48, 48, 48, 48, 48, 49, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00, 0x00
+        ]);
     }
 
     #[test]
@@ -1253,14 +1286,17 @@ mod test {
                 type_code: PayloadTypeCode::RoutingActivationRequest,
                 length: field::payload::ra_req::LENGTH as u32,
                 content: PayloadTypeContent::RoutingActivationRequest {
-                    source_address: 0,
+                    source_address: 0x0101,
                     activation_type: 0,
-                    iso_reserved: 0,
-                    oem_specific: 0,
+                    iso_reserved: 0xA0A0A0A0,
+                    oem_specific: 0xC0C0C0C0,
                 },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x00, 0x05, 0x00, 0x00, 0x00, 0x0B, 0x01, 0x01, 0x00, 0xA0, 0xA0, 0xA0, 0xA0, 0xC0, 0xC0, 0xC0, 0xC0
+        ]);
     }
 
     #[test]
@@ -1272,15 +1308,18 @@ mod test {
                 type_code: PayloadTypeCode::RoutingActivationResponse,
                 length: field::payload::ra_res::LENGTH as u32,
                 content: PayloadTypeContent::RoutingActivationResponse {
-                    logical_address_tester: 0,
-                    logical_address_doip_entity: 0,
+                    logical_address_tester: 0x0e80,
+                    logical_address_doip_entity: 0x0101,
                     routing_activation_response_code: 0,
-                    iso_reserved: 0,
-                    oem_specific: 0,
+                    iso_reserved: 0xBEEFBABE,
+                    oem_specific: 0xDEADBEA7,
                 },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x00, 0x06, 0x00, 0x00, 0x00, 0x0D, 0x0E, 0x80, 0x01, 0x01, 0x00, 0xBE, 0xEF, 0xBA, 0xBE, 0xDE, 0xAD, 0xBE, 0xA7
+        ]);
     }
 
     #[test]
@@ -1295,6 +1334,9 @@ mod test {
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00
+        ]);
     }
 
     #[test]
@@ -1305,10 +1347,13 @@ mod test {
             payload: Payload {
                 type_code: PayloadTypeCode::AliveCheckResponse,
                 length: 2,
-                content: PayloadTypeContent::AliveCheckResponse { source_address: 0 },
+                content: PayloadTypeContent::AliveCheckResponse { source_address: 0x0101 },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x00, 0x08, 0x00, 0x00, 0x00, 0x02, 0x01, 0x01
+        ]);
     }
 
     #[test]
@@ -1323,6 +1368,9 @@ mod test {
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x40, 0x01, 0x00, 0x00, 0x00, 0x00
+        ]);
     }
 
     #[test]
@@ -1335,13 +1383,16 @@ mod test {
                 length: 7,
                 content: PayloadTypeContent::DoIPEntityStatusResponse {
                     node_type: 0,
-                    max_open_sockets: 0,
+                    max_open_sockets: 2,
                     currently_open_sockets: 0,
-                    max_data_size: 0,
+                    max_data_size: 1024,
                 },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x40, 0x02, 0x00, 0x00, 0x00, 0x07, 0x00, 0x02, 0x00, 0x00, 0x00, 0x04, 0x00
+        ]);
     }
 
     #[test]
@@ -1356,6 +1407,9 @@ mod test {
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x40, 0x03, 0x00, 0x00, 0x00, 0x00
+        ]);
     }
 
     #[test]
@@ -1367,11 +1421,14 @@ mod test {
                 type_code: PayloadTypeCode::DiagnosticPowerModeInformationResponse,
                 length: 1,
                 content: PayloadTypeContent::DiagnosticPowerModeInformationResponse {
-                    diagnostic_power_mode: 0,
+                    diagnostic_power_mode: 1,
                 },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x40, 0x04, 0x00, 0x00, 0x00, 0x01, 0x01
+        ]);
     }
 
     #[test]
@@ -1383,13 +1440,16 @@ mod test {
                 type_code: PayloadTypeCode::DiagnosticMessage,
                 length: field::payload::dm::DATA(3).end as u32,
                 content: PayloadTypeContent::DiagnosticMessage {
-                    source_address: 0,
-                    target_address: 0,
+                    source_address: 0x0e80,
+                    target_address: 0x0101,
                     user_data: &[0x22, 0xF1, 0x90],
                 },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x80, 0x01, 0x00, 0x00, 0x00, 0x07, 0x0E, 0x80, 0x01, 0x01, 0x22, 0xF1, 0x90
+        ]);
     }
 
     #[test]
@@ -1401,14 +1461,17 @@ mod test {
                 type_code: PayloadTypeCode::DiagnosticMessageAck,
                 length: field::payload::dm_xack::DATA(0).end as u32,
                 content: PayloadTypeContent::DiagnosticMessagePositiveAcknowledgement {
-                    source_address: 0,
-                    target_address: 0,
+                    source_address: 0x0101,
+                    target_address: 0x0e80,
                     ack_code: 0,
                     previous_diagnostic_message: &[],
                 },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x80, 0x02, 0x00, 0x00, 0x00, 0x05, 0x01, 0x01, 0x0E, 0x80, 0x00
+        ]);
     }
 
     #[test]
@@ -1420,13 +1483,16 @@ mod test {
                 type_code: PayloadTypeCode::DiagnosticMessageNack,
                 length: field::payload::dm_xack::DATA(0).end as u32,
                 content: PayloadTypeContent::DiagnosticMessageNegativeAcknowledgement {
-                    source_address: 0,
-                    target_address: 0,
+                    source_address: 0x0101,
+                    target_address: 0x0e80,
                     nack_code: 0,
                     previous_diagnostic_message: &[],
                 },
             },
         };
         round_trip_test(repr);
+        round_trip_test_with_bytes(repr, &[
+            0x02, 0xfd, 0x80, 0x03, 0x00, 0x00, 0x00, 0x05, 0x01, 0x01, 0x0E, 0x80, 0x00
+        ]);
     }
 }
